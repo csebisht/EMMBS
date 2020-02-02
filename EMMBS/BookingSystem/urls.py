36b4926c0ef5c630d9b21a1a72f1b614d1a79070
csebisht93 <csebisht@gmail.com>
2020-02-02 18:24:10 +0530

    commit

--- EMMBS/BookingSystem/urls.py
+++ EMMBS/BookingSystem/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('MeetingList/', views.MeetingList, name='MeetingList'),
+    path('BookMeeting/', views.BookMeeting, name='BookMeeting'),
 
 ]
--- EMMBS/BookingSystem/views.py
+++ EMMBS/BookingSystem/views.py
@@ -1,8 +1,65 @@
-from django.shortcuts import render
-from .models import Room,RoomEquipment
+from django.shortcuts import render,redirect
+from .models import Room,RoomEquipment,Meeting
+from django.contrib import messages
 # Create your views here.
 def index(request):
     room = Room.objects.all()
     roomequip=RoomEquipment.objects.all()
     print(room)
     return render(request, "index.html", {"Room_detail": room,"roomEquipment":roomequip})
+
+
+def BookMeeting(request):
+    if request.method == 'POST':
+        MeetingDate = request.POST['MeetingDate']
+        StartTime = request.POST['StartTime']
+        EndTime = request.POST['EndTime']
+        Purpose = request.POST['Purpose']
+        RoomId = request.POST['RoomId']
+        #meeting = Meeting(request.POST)
+        meeting = Meeting.objects.create(EmployeeId=1,MeetingDate=MeetingDate, StartTime=StartTime, EndTime=EndTime, Purpose=Purpose,RoomId=RoomId,CreationDate=MeetingDate,CreationBy=1)
+        meeting.save()
+        if meeting is not None:
+            return redirect('/')
+        else:
+            messages.info(request, "Invalid Credentials")
+            return redirect('MeetingList')
+    return render(request ,"MeetingList.html")
+
+def MeetingList(request):
+    if request.method == 'POST':
+        MeetingDate = request.POST['MeetingDate']
+        StartTime = request.POST['StartTime']
+        EndTime = request.POST['EndTime']
+        room = Room.objects.all()
+        roomequip = RoomEquipment.objects.all()
+        for meeting in Meeting.objects.filter(MeetingDate=MeetingDate).filter(StartTime=StartTime).filter(EndTime=EndTime):
+            print("meeting  " ,meeting)
+            for Rroom in room:
+                print("Room ",Rroom)
+                if Rroom.id == meeting.roomId:
+                     print("asd")
+                else:
+                     print("asdasd")
+        if Meeting.objects.filter(MeetingDate=MeetingDate).filter(StartTime=StartTime).filter(EndTime=EndTime).exists():
+            messages.info(request, "Meeting Booked Already")
+            return redirect('/')
+        else:
+            return render(request,'MeetingList.html',{"Room_detail": room,"roomEquipment":roomequip,"MeetingDate":MeetingDate,"StartTime":StartTime,"EndTime":EndTime})
+    return  redirect('/')
+
+def MeetingList12(request):
+    if request.method == 'POST':
+        MeetingDate = request.POST['MeetingDate']
+        StartTime = request.POST['StartTime']
+        EndTime = request.POST['EndTime']
+        room = Room.objects.all()
+        roomequip = RoomEquipment.objects.all()
+
+        if Meeting.objects.filter(MeetingDate=MeetingDate).filter(StartTime=StartTime).filter(EndTime=EndTime).exists():
+            messages.info(request, "Meeting Booked Already")
+            return redirect('/')
+        else:
+            Meeting.object.get()
+            return render(request,'MeetingList.html',{"Room_detail": room,"roomEquipment":roomequip,"MeetingDate":MeetingDate,"StartTime":StartTime,"EndTime":EndTime})
+    return  redirect('/')
